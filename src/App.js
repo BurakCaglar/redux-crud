145a357ad7f09a3523c0e0208325c624557dfe11
BurakCaglar <burakcaglar@yandex.com>
2021-05-18 03:24:12 +0300

    addpost, editpost

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import {
   Redirect,
 } from 'react-router-dom'
 import { Navbar } from './app/Navbar'
+import AddPostForm from './features/posts/AddPostForm'
 import { PostsList } from './features/posts/PostsList'
+import SinglePostPage from './features/posts/SinglePostPage'
+import EditPostForm from './features/posts/EditPostForm'
 
 function App() {
   return (
@@ -19,10 +22,13 @@ function App() {
             path="/"
             render={() => (
               <React.Fragment>
+                <AddPostForm />
                 <PostsList />
               </React.Fragment>
             )}
           />
+          <Route exact path="/posts/:postId" component={SinglePostPage} />
+          <Route exact path="/editPost/:postId" component={EditPostForm} />
           <Redirect to="/" />
         </Switch>
       </div>
